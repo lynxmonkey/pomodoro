25629e20fc1fed5e11a75c922c94eeead1a56adf
rodion <rodionaaafgh@gmail.com>
2018-12-15 06:40:24 +0300

    feature: enable service worker

--- front/src/components/page.js
+++ front/src/components/page.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export default styled.div`
   position: relative;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
