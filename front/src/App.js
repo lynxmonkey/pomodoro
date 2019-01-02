f6c076722a60fbfe722215c99f14ff2095a6ebca
rodion <rodionaaafgh@gmail.com>
2019-01-02 06:35:03 +0300

    update: yandex analytics -> google analytics

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/browser'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { Provider } from 'react-redux'
-import { YMInitializer } from 'react-yandex-metrika'
+import ReactGA from 'react-ga'
 
 import saga from './sagas'
 import store from './store'
@@ -11,26 +11,11 @@ import Layout from './components/layout'
 import { THEME } from './constants/theme'
 
 export default () => (
-  <>
-    <Provider store={store}>
-      <ThemeProvider theme={THEME}>
-        <Layout/>
-      </ThemeProvider>
-    </Provider>
-    {process.env.NODE_ENV === 'production' && (
-      <YMInitializer
-        accounts={[51491761]}
-        version="2"
-        options={{
-          id: 51491761,
-          clickmap: true,
-          trackLinks: true,
-          accurateTrackBounce: true,
-          webvisor: true
-        }}
-      />
-    )}
-  </>
+  <Provider store={store}>
+    <ThemeProvider theme={THEME}>
+      <Layout/>
+    </ThemeProvider>
+  </Provider>
 )
 
 sagaMiddleware.run(saga)
@@ -39,7 +24,8 @@ sagaMiddleware.run(saga)
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: "https://0dc467f02c2a46c9bfaacff3bab0b6ef@sentry.io/1362069"
-   })
+  })
+  ReactGA.initialize('UA-131566304-1')
 }
 
 
