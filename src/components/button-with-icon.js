45ae12ae14cfcfb97fea555a50617aba39a095d3
rodion <rodionaaafgh@gmail.com>
2019-03-01 12:13:04 +0300

    process: auth buttons

---
+++ src/components/button-with-icon.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { centerContentStyle } from 'increaser-components'
+
+const Container = styled.button`
+  color: ${p => p.theme.color.mainFont};
+  margin: 10px 0;
+  height: 40px;
+  border-radius: 20px;
+  border: 2px solid #31CEFF;
+  ${centerContentStyle};
+  background-color: transparent;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+  width: 240px;
+  &:hover {
+    background-color: rgba(52, 152, 219, 0.15);
+  }
+`
+
+const IconPart = styled.div`
+  width: 18%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`
+
+const TextPart = styled.p`
+  width: 92%;
+  padding-left: 10px;
+  font-size: 16px;
+  display: flex;
+  flex-direction: row;
+`
+
+export default ({ icon, text, onClick }) => {
+  return (
+    <Container onClick={onClick}>
+      <IconPart>
+        <FontAwesomeIcon size={'lg'} icon={icon} />
+      </IconPart>
+      <TextPart>{text}</TextPart>
+    </Container>
+  )
+}
--- src/components/sync.js
+++ src/components/sync.js
@@ -1,49 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
-import { RoundButton } from 'increaser-components'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGoogle, faFacebookF } from '@fortawesome/free-brands-svg-icons'
 
 import * as actions from '../actions/auth'
 import { connectTo, takeFromState } from '../utils/generic'
-
+import Button from './button-with-icon'
 
 const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-direction: row;
-  height: 60px;
-  color: ${p => p.theme.color.mainFont};
+  flex-direction: column;
   margin-top: 10px;
-`
-
-const ButtonWrap = styled.div`
-  margin: 5px;
-`
-
-const Text = styled.p`
-  font-size: 18px;
 `
 
 const Sync = ({ token, authorizeWithGoogle, authorizeWithFacebook }) => {
   if (token || !window.navigator.onLine) return null
-
   return (
     <Container>
-      <Text>Sign in with</Text>
-      <ButtonWrap>
-        <RoundButton size='s' type='default' onClick={authorizeWithGoogle}>
-          <FontAwesomeIcon size={'lg'} icon={faGoogle} />
-        </RoundButton>
-      </ButtonWrap>
-      <Text> or </Text>
-      <ButtonWrap>
-        <RoundButton size='s' type='default' onClick={authorizeWithFacebook}>
-          <FontAwesomeIcon size={'lg'} icon={faFacebookF} />
-        </RoundButton>
-      </ButtonWrap>
+      <Button onClick={authorizeWithGoogle} icon={faGoogle} text='Sign In with Google'/>
+      <Button onClick={authorizeWithFacebook} icon={faFacebookF} text='Sign In with Facebook'/>
     </Container>
   )
 }
