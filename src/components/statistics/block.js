bb5f284d6b653334535b02d3d9a7f52cccd806e0
rodion <rodionaaafgh@gmail.com>
2019-04-05 07:26:07 +0300

    process: statistics: chart

---
+++ src/components/statistics/block.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import { secondsFormatter } from '../../utils/time';
+
+const Container = styled.div`
+  width: 400px;
+  height: 240px;
+  padding: 10px;
+  margin: 20px;
+  border-radius: 5px;
+  background-color: ${p => p.theme.color.default};
+`
+
+const Header = styled.div`
+  height: 40px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`
+
+const Text = styled.p`
+  color: ${p => p.theme.color.mainFont};
+`
+
+export default ({ period, time, children }) => {
+  return (
+    <Container>
+      <Header>
+        <Text>{period}: {secondsFormatter(time)}</Text>
+      </Header>
+      {children}
+    </Container>
+  )
+}
--- src/components/statistics/index.js
+++ src/components/statistics/index.js
@@ -1,15 +1,62 @@
 import React from 'react'
 import DocumentTitle from 'react-document-title'
+import styled from 'styled-components'
+import { DateTime } from 'luxon'
+import { BarChart } from 'increaser-charts'
 
 import { connectTo, takeFromState } from '../../utils/generic';
 import PageWithExit from '../page-with-exit'
+import Block from './block'
+import { THEME } from '../../constants/theme';
+import { toTime } from '../../utils/time';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`
+
+const WEEKDAYS = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+]
+
+const setsToSeconds = sets => sets.reduce((acc, set) => acc + set.end - set.start, 0) / 1000
 
 const Statistics = ({ sets }) => {
-  console.log(sets)
+  const total = setsToSeconds(sets)
+  const weekday = DateTime.local().weekday
+  const sums = Array(7).fill(0).map((__, index) => {
+    if (index >= weekday) return 0
+    const weekdaySets = sets.filter(s => toTime(s.start).weekday === index + 1)
+    return setsToSeconds(weekdaySets)
+  })
+  const bars = Array(7).fill(0).map((__, index) => ({
+    label: WEEKDAYS[index],
+    items: [{
+      color: THEME.color.gold,
+      value: sums[index]
+    }]
+  }))
   return (
     <DocumentTitle title='statistics'>
       <PageWithExit>
-        
+        <Container>
+          <Block period='Week' time={total}>
+            <BarChart
+              bars={bars}
+              barWidth={42}
+              barSpace={4}
+              showScroll={false}
+              selectCenterBarOnScroll={false}
+            />
+          </Block>
+        </Container>
       </PageWithExit>
     </DocumentTitle>
   )
